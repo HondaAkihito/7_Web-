f5220390674c6c9e32f1e16d5fa695a6a7fc3b3f
honaki-days <ice.make.sky.sakura@icloud.com>
2024-03-09 13:21:52 +0900

    “20240308-「デザイン_トップページ_再作成」-作成完了”

---
+++ app/resources/js/components/TopComponent.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="container-fluid mb-3">
+        <div class="container">
+            <nav class="navbar">
+                <table class="table border">
+                  <thead>
+                    <tr>
+                      <th scope="col" class="text-center">残り期間</th>
+                      <th scope="col" class="text-center">残り予算</th>
+                      <th scope="col" class="text-center">1日あたり</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td class="text-center">○~○</td>
+                      <td class="text-center">0円</td>
+                      <td class="text-center">○円 / day</td>
+                    </tr>
+                  </tbody>
+                </table>
+            </nav>
+        </div>
+    </div>
+
+</template>
+
+<script>
+    export default {}
+</script>
